42cca76df1d522101cbe0241e702aa35c1bfb1fc
Phi Viet <phiviet.ff@gmail.com>
2018-07-21 23:30:53 +0700

    turn off log

--- src/app/pipe/sort.pipe.ts
+++ src/app/pipe/sort.pipe.ts
@@ -10,32 +10,9 @@ export class SortPipe implements PipeTransform {
 
         let temp = [];
         array.forEach(element => {
-            console.log(element.img);
             temp.push(Number(element.img));
         });
-        console.log(temp);
         temp.sort((a, b) => a - b);
-        // temp.sort((a, b) => b -a);
-
-        console.log(temp);
-
         return temp;
     }
 }
-
-/**
- *  transform(array: Array<any>, args: string): Array<any> {
-        // Do YOUR LOGIC HERE, like this code below
-
-        array.sort((a: any, b: any) => {
-            if (a < b) {
-                return -1;
-            } else if (a > b) {
-                return 1;
-            } else {
-                return 0;
-            }
-        });
-        return array;
-    }
- */
