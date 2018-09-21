da449b171786b77f86699fb14fea46341afb4e34
phiviet <phiviet.ff@gmail.com>
2018-09-21 14:38:39 +0700

    Update chat.service.ts

--- src/app/chat.service.ts
+++ src/app/chat.service.ts
@@ -10,8 +10,8 @@ export class ChatService {
     private socket;
     private username;
 
-    // private url = 'https://app-vietchat.herokuapp.com';
-    private url = 'http://localhost:8080';
+    private url = 'https://app-vietchat.herokuapp.com';
+    //private url = 'http://localhost:8080';
     constructor(private router: Router,) {
         this.socket = io(this.url);
         // this.listOnlineUsersFromSoket().subscribe(data => {
